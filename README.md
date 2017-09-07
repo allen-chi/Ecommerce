ebaa0c6fc61e49c1a58b71d992272904ac044ddf
allen-chi <allen.chinazor@yahoo.com>
2017-09-07 07:23:23 +0100

    chore(environment-setup): Setup front-end environment with necessary tools
    - config eslint, babel, webpack, express server, test setup  and npm scritps
    Finishes[#147620753]

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ecommerce
+Learn Angular 4 and Laravel by building an Ecommerce web
---
+++ src/tests/index.test.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('First test demo', () => {
+  it('should pass', () => {
+    expect(true).toEqual(true);
+  });
+});
